b335822ba2dcb5b30fb5b8796da1fbb83ca92de5
devdharmender <dheetdev@gmail.com>
2025-11-28 22:34:05 +0530

    adding style

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,14 +7,14 @@ export default function About() {
       <section id="about" className="py-16 md:py-32  text-white bg-[#04081A]">
         <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
           <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-4xl text-white">
-            Software Developer ~ Laravel
+            Software Developer - Laravel
           </h2>
           <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
             <div className="relative mb-6 sm:mb-0">
-              <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl p-px from-zinc-300 to-transparent">
+              <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl p-px from-zinc-300 to-transparent border border-white border-2">
                 <img
                   src={HeroImg}
-                  className="rounded-[15px] shadow block"
+                  className="rounded-[15px] shadow block p-2"
                   alt="payments illustration"
                   width={1207}
                   height={929}
